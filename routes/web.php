1d8f083f1efab93c758a0d0a9a427742f226e84e
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2023-11-07 15:51:45 -0300

    feat: wip

--- routes/web.php
+++ routes/web.php
@@ -52,5 +52,5 @@ Route::put('/update/{id_contact}', function (Request $informacoes, $id_contact) 
     $contact->email = $informacoes->email_contact;
     $contact->contact = $informacoes->number_contact;
     $contact->save();
-    return redirect('/mostrar-contacts')->with('mensagem', 'Candidato atualizado com sucesso!');
+    return redirect('/')->with('mensagem', 'Contact Update Success!');
 });
