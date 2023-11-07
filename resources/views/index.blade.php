b8229fc779203508c798105db29835ff84543e43
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2023-11-07 15:01:31 -0300

    feaer: wip

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,10 +9,27 @@
 <body>
     <h1>Contacts List</h1>
 
-    <ul>
-        @foreach ($contacts as $contact)
-            
-        @endforeach
-    </ul>
+   @if($contacts->isEmpty())
+                <p>Contact List is Empty.</p>
+                <br>
+                <button onclick="window.location.href='/'" class="btn btn-warning">
+                    Cadastrar novos jogadores
+                </button>
+            @else
+            @foreach($contacts as $contact)
+                <div>
+                    <p>Nome: {{ $contact->name }}, Posição: {{ $contact->telefone }}</p> 
+                    <form action="/excluir-contact/{{ $contact->id }}" method="POST" style="display: inline-block;">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-danger">Excluir</button>
+                    </form>
+                    
+                    <form action="/editar-contacts/{{ $contact->id }}" method="GET" style="display: inline-block;">
+                        <button type="submit" class="btn btn-warning" onclick="mostrarFormEditar()">Editar</button>
+
+                    </form>
+                </div>
+            @endforeach
 </body>
 </html>
