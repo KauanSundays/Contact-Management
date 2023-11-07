460a10db9c31cc800e41415b43c0097bf8e9ccec
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2023-11-07 17:01:38 -0300

    wip

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -3,31 +3,33 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie-edge">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{ asset('index.css') }}">
-    <title>Edit Contact</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="{{asset('index.css')}}">
+    <title>CRUD</title>
 </head>
-<body class="d-flex flex-column align-items-center justify-content-center vh-100" style="background: chocolate">
-    <h1 class="mb-4">Edit Contact: {{ $contact->name }}</h1>
+<body class="d-flex flex-column align-items-center justify-content-center vh-100">
+    <h1 class="mb-4">Lista de Jogadores</h1>
 
-    <div class="container-box text-center">
-        <form action="/update/{{ $contact->id }}" method="POST">
+    
+    <div id class="container-box text-center">
+        <form action="/atualizar/{{ $contact->id }}" method="POST">
             @csrf
             @method("PUT")
             <div class="text-left">
-                <label for="name_contact">Nome:</label>
-                <input type="text" placeholder="name_contact" name="name_contact" value="{{ $contact->name }}">
+                <label for="">Name:</label>
+                <input type="text" placeholder="nome_contact" name="name_contact" value="{{$contact->name}}">
             </div>
             <br>
             <div class="text-left">
-                <label for="email_contact">Email:</label>
-                <input type="text" placeholder="email_contact" name="email_contact" value="{{ $contact->email }}">
-            </div>            
+                <label for="">Number:</label>
+                <input type="text" placeholder="number_contact" name="number_contact" value="{{$contact->number}}">
+            </div>
             <br>
             <div class="text-left">
-                <label for="number_contact">Contact:</label>
-                <input type="text" placeholder="contact_Number" name="number_contact" value="{{ $contact->contact }}">
+                <label for="">Email:</label>
+                <input type="text" placeholder="email_contact" name="email_contact" value="{{$contact->email}}">
             </div>
             <br>
             <div class="text-left">
@@ -35,5 +37,7 @@
             </div>
         </form>
     </div>
+    
+
 </body>
 </html>
