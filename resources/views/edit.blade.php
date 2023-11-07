fe5a3230433dd23769b1c460373d0e0490b02680
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2023-11-07 17:28:01 -0300

    wip

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="X-UA-Compatible" content="ie-edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="{{asset('index.css')}}">
@@ -34,10 +34,9 @@
             <br>
             <div class="text-left">
                 <button class="btn btn-success">Enviar Cadastro</button>
+                <a href="/" class="btn btn-primary">Retornar à Lista de Contatos</a>
             </div>
         </form>
     </div>
-    
-
 </body>
 </html>
